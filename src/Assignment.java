ecde98e712d2837fd46f1c5023a347013a7c4925
Kevin Ding <dingym@yahoo.com>
2012-10-06 18:08:33 -0400

    Finished homework 5

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,12 +1,40 @@
+import java.util.Date;
 
 public class Assignment {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Date toDay = new Date();
+		Task aTask = new Task("Homework", "Do BMA5", toDay);
+		System.out.println(aTask);
+		Task aTask2 = new Task();
+		System.out.println(aTask2);
+	} //end of main()
 
+    public static String getElement(String values[], int index) {
+    	if(index >=0 && index<=values.length) {
+    		return values[index];
+    	} 
+    	else {
+    	    return null;	
+    	}
+    } // end of getElement()
+} // end of class Assignment
+class Task {
+	String title;
+	String desc;
+	Date date = new Date();
+	Task(String title, String desc, Date date){
+		this.title = title;
+	    this.desc = desc;
+		this.date = date;
 	}
-
-}
+	Task(){
+		//Date toDay = new Date();
+		this.title = "No title";
+	    this.desc = "No description";
+	    //this.date = toDay;
+	}
+	public String toString(){
+		return "Task(\nTitle: "+ title + ". \nDescription: "+desc +". \nCreated at: " +date.toString()+"\n)\n";
+	}} //end of Task
